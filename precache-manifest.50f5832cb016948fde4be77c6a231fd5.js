8113ebb087a6e2346b9279f67876d3da4bd99fcf
allreddy <nithin@enplug.com>
2019-01-10 19:06:51 -0800

    Updates

---
+++ precache-manifest.50f5832cb016948fde4be77c6a231fd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85788ee6d453ba088fce",
+    "url": "/static/css/main.d9bddd88.chunk.css"
+  },
+  {
+    "revision": "85788ee6d453ba088fce",
+    "url": "/static/js/main.85788ee6.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29cff561fd6f53a1704e2462e4c92199",
+    "url": "/index.html"
+  }
+];
